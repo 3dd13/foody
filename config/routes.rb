d911e4523c23580da33a878460e144785f7efc41
Eddie Lau 3dd13 <tatonlto@gmail.com>
2017-07-11 22:19:52 +0800

    generated contact and about pages. generated root route to home index. generated restaurants page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'contact', to: 'pages#contact'
+
+  get 'about', to: 'pages#about'
+
+  root to: 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
